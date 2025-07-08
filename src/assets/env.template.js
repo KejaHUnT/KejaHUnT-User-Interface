ab6096c960ed6d98114eefd1f772a4a89f38c97a
Kalukwo <123067924+Kalukwo@users.noreply.github.com>
2025-07-08 17:30:36 +0300

    Create env.template.js

---
+++ src/assets/env.template.js
@@ -0,0 +1,8 @@
+(function (window) {
+  window._env = window._env || {};
+  window._env.API_BASE_URL = '${API_BASE_URL}';
+  window._env.TENANT_API_BASE_URL = '${TENANT_API_BASE_URL}';
+  window._env.FILE_HANDLER_API_BASE_URL = '${FILE_HANDLER_API_BASE_URL}';
+  window._env.BOOKING_API_BASE_URL = '${BOOKING_API_BASE_URL}';
+  window._env.PAYMENT_API_BASE_URL = '${PAYMENT_API_BASE_URL}';
+})(this);
