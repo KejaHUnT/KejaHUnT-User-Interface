a992f6c224a76daa0cccce4aa0aa07f3dcfb8f76
Karol <kiptoocarroll@gmail.com>
2025-07-18 18:48:40 +0300

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
-  apiBaseUrl: (window as any)._env?.API_BASE_URL || 'http://localhost:7209',
+  apiBaseUrl: (window as any)._env?.API_BASE_URL || 'http://localhost:7102/properties',
   tenantApiBaseUrl: (window as any)._env?.TENANT_API_BASE_URL || 'https://localhost:7159',
   fileHandlerApiBaseUrl: (window as any)._env?.FILE_HANDLER_API_BASE_URL || 'https://localhost:7115',
   bookingApiBaseUrl: (window as any)._env?.BOOKING_API_BASE_URL || 'https://localhost:7160',
