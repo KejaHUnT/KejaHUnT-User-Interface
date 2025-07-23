90ce38999fbcfd3d24751263c9e5417b302d00db
kalukwo <kaldrack789@gmail.com>
2025-07-23 13:39:00 +0300

    add env refresh lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigService } from './services/config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'KejaHUnT';
+
+  constructor(private configService: ConfigService) {}
+
+  ngOnInit() {
+    // Start monitoring config changes when app loads
+    this.configService.startConfigMonitoring();
+  }
 }
