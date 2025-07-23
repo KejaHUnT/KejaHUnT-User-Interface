22ca8abb7cb84e9842e8ab609caf9d68b7418fc5
Karol <kiptoocarroll@gmail.com>
2025-07-23 13:59:19 +0300

    update

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+interface Window {
+  _env: {
+    API_BASE_URL: string;
+    TENANT_API_BASE_URL: string;
+    FILE_HANDLER_API_BASE_URL: string;
+    BOOKING_API_BASE_URL: string;
+    PAYMENT_API_BASE_URL: string;
+    ACCESS_API_BASE_URL: string;
+    [key: string]: string;
+  };
+}
