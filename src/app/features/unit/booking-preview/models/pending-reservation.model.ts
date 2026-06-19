aac18f8db9a5e0a2406c566ea6117fc1f6bf36ec
Kalukwo <kaldrack789@gmail.com>
2026-06-20 01:39:32 +0300

    Fix unit to show door number as main unit idenitifier

--- src/app/features/unit/booking-preview/models/pending-reservation.model.ts
+++ src/app/features/unit/booking-preview/models/pending-reservation.model.ts
@@ -2,6 +2,7 @@ export interface PendingReservation {
     bookingId: number;
     bookingReference: string;
     unitId: number;
+    unitDoorNumber: string;
     tenantId: number;
     tenantName: string;
     tenantPhone: string;
